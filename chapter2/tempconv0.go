36b9938bd36e944dabbfabb6e3667c1a3e7a49cc
marabesi <matheus.marabesi@gmail.com>
2021-09-09 07:54:47 +0200

    chore(chapter 2): type example commented

--- chapter2/tempconv0.go
+++ chapter2/tempconv0.go
@@ -3,13 +3,13 @@ package tempconv
 type Celsius float64
 type Fahrenheit float64
 
-const (
-	AbsoluteZeroC Celsius = -273.15
-	FreezingC
-	Celsius = 0
-	BoilingC
-	Celsius = 100
-)
+//const (
+//	AbsoluteZeroC celsius = -273.15
+//	FreezingC
+//	celsius = 0
+//	BoilingC
+//	celsius = 100
+//)
 
 func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
 
